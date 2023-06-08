eb2f2a7745f95bcb19e3134910f6d153da44ca29
tuananh-e <savanttkdt@gmail.com>
2023-06-08 13:24:27 +0700

    thêm bảng image của product và color

---
+++ backend/Backend-NikeStore/Persistence/Configurations/ProductImageConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Configurations
+{
+    public class ProductImageConfiguration : IEntityTypeConfiguration<ProductImage>
+    {
+        public void Configure(EntityTypeBuilder<ProductImage> builder)
+        {
+            builder.ToTable(nameof(ProductImage));
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.ImageUrl).IsRequired();
+            builder.Property(p => p.SetAsDefault).IsRequired(false);
+
+            builder.HasOne(p => p.Color)
+                   .WithMany(p => p.ProductImages)
+                   .HasForeignKey(p => p.ColorId);
+
+            builder.HasOne(p => p.Product)
+                   .WithMany(p => p.ProductImages)
+                   .HasForeignKey(p => p.ProductId);
+        }
+    }
+}
