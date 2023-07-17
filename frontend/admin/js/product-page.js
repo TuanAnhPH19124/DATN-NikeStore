9be03033b79884a1e7e1df6445224dc401720fcc
HaCodeSpace <nguyenvietha2ka2@gmail.com>
2023-07-17 10:16:01 +0700

    fake api and add event

--- frontend/admin/js/product-page.js
+++ frontend/admin/js/product-page.js
@@ -1,3 +1,33 @@
 function myFunction() {
-    window.location.href = "/frontend/admin/product-detail.html";
+    console.log("xóa thành công");
 }
+// call api with jquery
+$(document).ready(function () {
+    $('#productData').DataTable({
+        "ajax": {
+            "url": "https://6447750e50c253374425338d.mockapi.io/fake",
+            "dataType": "json",
+            "dataSrc": ""
+        },
+        "columns": [
+            { "data": 'image' },
+            { "data": 'name' },
+            { "data": 'price' },
+            { "data": 'createdPrice' },
+            { "data": 'status' },
+            {
+                "render": function () {
+                    return '<td><a class="btn btn-primary" id="btn" onclick="myFunction()">Xóa</a></td>';
+                }
+            },
+        ],
+    });
+});
+//add event click datatable
+
+$('#productData tbody').on('click', 'tr', function (event) {
+    if ($(event.target).is('td')) {
+        window.location.href = "/frontend/admin/product-detail.html";
+    }
+});
+
