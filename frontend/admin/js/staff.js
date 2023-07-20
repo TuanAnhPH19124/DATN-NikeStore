40f51ba5a3ee65a493fd1d8e7e772fddc819ff5b
HaCodeSpace <nguyenvietha2ka2@gmail.com>
2023-07-20 10:50:51 +0700

    validate add staff

--- frontend/admin/js/staff.js
+++ frontend/admin/js/staff.js
@@ -46,4 +46,58 @@ $(document).ready(function () {
             }
         });
     });
+    // custom validate 
+    $.validator.addMethod("nameContainOnlyChar", function (value, element) {
+        return value.match(/[^a-zA-Z]/) == null;
+    });
+    $.validator.addMethod("idContainOnlyNum", function (value, element) {
+        return value.match(/[^0-9]/) == null;
+    });
+    $.validator.addMethod("phoneNumContainOnlyNum", function (value, element) {
+        return value.match(/[^0-9]/) == null;
+    });
+    $.validator.addMethod("onlyContain12Char", function (value, element) {
+        return value.match(/^\w{12}$/) != null;
+    });
+    // add validate
+    $("#add-employee-form").validate({
+        rules: {
+            "fullName": {
+                required: true,
+                maxlength: 30,
+                nameContainOnlyChar: true,
+            },
+            "snn": {
+                required: true,
+                idContainOnlyNum: true,
+                onlyContain12Char: true,
+            },
+            "phoneNumber": {
+                require: true,
+                phoneNumContainOnlyNum: true,
+            },
+            "role": {
+                require: true,
+            }
+        },
+        messages: {
+            "fullName": {
+                required: "Bạn phải nhập họ và tên",
+                maxlength: "Hãy nhập tối đa 30 ký tự",
+                nameContainOnlyChar: "Họ tên không được chứa số hay ký tự",
+            },
+            "snn": {
+                required: "Bạn phải nhập số căn cước",
+                idContainOnlyNum: "Số căn cước không được chưa ký tự",
+                onlyContain12Char: "Độ dài của số căn cước là 12"
+            },
+            "phoneNumber": {
+                required: "Bạn phải nhập số điện thoại",
+                phoneNumContainOnlyNum: "Số điện thoại không được chứa ký tự"
+            },
+            "role": {
+                required: "Bạn phải nhập tên vai trò",
+            }
+        },
+    });
 });
