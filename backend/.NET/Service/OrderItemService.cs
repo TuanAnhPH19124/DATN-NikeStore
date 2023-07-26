95402fb2fc8539588d2f4d02fcb6afc5afd7da9f
Trần Trung Phong <phongtt2710@gmail.com>
2023-07-26 22:39:05 +0700

    update thong ke

---
+++ backend/.NET/Service/OrderItemService.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Repositories;
+using Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Service.Abstractions;
+
+namespace Service
+{
+    public class OrderItemService : IOrderItemService
+    {
+        private readonly IRepositoryManger _repositoryManger;
+
+        public OrderItemService(IRepositoryManger repositoryManger)
+        {
+            _repositoryManger = repositoryManger;
+        }
+
+        // So luong ban cua 1 sp
+        public async Task<int> GetAmountByProductId(string productId)
+        {
+            return await _repositoryManger.OrderItemsRepository.GetAmountByProductId(productId);
+        }
+
+        // thong ke doanh thu cua 1 sp
+        public async Task<decimal> GetRevenueByProductId(string productId)
+        {
+            return await _repositoryManger.OrderItemsRepository.GetRevenueByProductId(productId);
+        }
+
+        // thong ke tong doanh thu
+        public async Task<decimal> GetTotalRevenueInTimeRange(DateTime startDate, DateTime endDate)
+        {
+            return await _repositoryManger.OrderItemsRepository.GetTotalRevenueInTimeRange(startDate, endDate);
+        }
+    }
+}
