f52d396e0b188b2b42dae48e1abc7f52f19f3585
HaCodeSpace <nguyenvietha2ka2@gmail.com>
2023-08-06 15:13:58 +0700

    fix api

--- backend/.NET/Domain/Entities/Product.cs
+++ backend/.NET/Domain/Entities/Product.cs
@@ -12,20 +12,20 @@ namespace Domain.Entities
     [Table("Products")]
     public class Product : BaseEntity
     {
-        [JsonIgnore]
+    
         public string BarCode { get; set; }
-        [JsonIgnore]
+   
         public double CostPrice { get; set; } 
         public double RetailPrice { get; set; }
         public string Description { get; set; }
         public Brands Brand { get; set; } = Brands.Nike;    
         public Status Status { get; set; }
         public int DiscountRate { get; set; } = 1;
-        public virtual List<Category> Categories { get; set; } = new List<Category>();
-        public virtual List<Tag> Tags { get; set; } = new List<Tag>();
-        public virtual List<Stock> Stocks { get; set; } = new List<Stock>();
-        public virtual List<ProductImage> ProductImages { get; set; } = new List<ProductImage>();
-        public virtual IEnumerable<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+        public virtual List<Category> Categories { get; set; } 
+        public virtual List<Tag> Tags { get; set; } 
+        public virtual List<Stock> Stocks { get; set; } 
+        public virtual List<ProductImage> ProductImages { get; set; } 
+        public virtual IEnumerable<OrderItem> OrderItems { get; set; } 
         public ShoppingCartItems ShoppingCartItems { get; set; }
         public ICollection<ProductRate> ProductRate { get; set; }
         public virtual List<CategoryProduct> CategoryProducts { get; set; }
