11df9b7694fca803b52475d9e587c33c4d75879e
Trần Trung Phong <phongtt2710@gmail.com>
2023-08-27 20:39:20 +0700

    add product api load 1 ảnh ở table

--- backend/.NET/Service/ProductService.cs
+++ backend/.NET/Service/ProductService.cs
@@ -177,6 +177,51 @@ namespace Service
             return productDTOs;
         }
 
+        public async Task<List<ProductDtoForGet>> GetAllProductImageAsync(CancellationToken cancellationToken)
+        {
+            var products = await _repositoryManger.ProductRepository.GetAllProductImageAsync();
+
+            var productDTOs = products.Select(product => new ProductDtoForGet
+            {
+                Id = product.Id,
+                Name = product.Name,
+                BarCode = product.BarCode,
+                RetailPrice = product.RetailPrice,
+                Description = product.Description,
+                Status = product.Status,
+                DiscountRate = product.DiscountRate,
+                SoleId = product.SoleId,
+                MaterialId = product.MaterialId,
+
+                Stocks = product.Stocks.Select(stock => new StockDto
+                {
+                    SizeId = stock.SizeId,
+                    ColorId = stock.ColorId,
+                    UnitInStock = stock.UnitInStock,
+                    ProductId = stock.ProductId
+                }).ToList(),
+
+                CategoryProducts = product.CategoryProducts.Select(categoryProduct => new CategoryProductDto
+                {
+                    ProductId = categoryProduct.ProductId,
+                    CategoryId = categoryProduct.CategoryId
+                    // Sao chép các thuộc tính khác từ categoryProduct
+                }).ToList(),
+
+                ProductImages = product.ProductImages.Select(image => new ProductImageDto
+                {
+                    Id = image.Id,
+                    ImageUrl = image.ImageUrl,
+                    SetAsDefault = image.SetAsDefault,
+                    ProductId = image.ProductId,
+                    ColorId = image.ColorId
+                    // Sao chép các thuộc tính khác từ image
+                }).ToList()
+            }).ToList();
+
+            return productDTOs;
+        }
+
         Task<Product> IProductService.GetByIdProduct(string id, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
--- frontend/admin/js/product-page.js
+++ frontend/admin/js/product-page.js
@@ -1,95 +1,115 @@
 function myFunction() {
-    console.log("xóa thành công");
+  console.log("xóa thành công");
 }
 // call api with jquery
 $(document).ready(function () {
-    $('#productData').DataTable({
-        "ajax": {
-            "url": "https://localhost:44328/api/Product",
-            "dataType": "json",
-            "dataSrc": ""
+  $("#productData").DataTable({
+    ajax: {
+      url: "https://localhost:44328/api/Product/1Image",
+      dataType: "json",
+      dataSrc: "",
+    },
+    columns: [
+      {
+        data: "id",
+        title: "STT",
+        render: function (data, type, row, meta) {
+          return meta.row + 1;
         },
-        "columns": [
-            {
-                "data": 'id', 'title': 'STT', render: function (data, type, row, meta) {
-                    return meta.row + 1;
-                }
-            },
-            { "data": 'productImages[0].imageUrl', 'title': 'Ảnh',
-            "render": function (data, type, row) {
-                    return `<img src="https://localhost:44328/${data}" alt="" style="border-radius: 10%;" width=120px height=110px>`;
-            }},
-            { "data": 'name', 'title': 'Tên sản phẩm' },
-            { "data": 'retailPrice', 'title': 'Giá bán',
-            "render": function (data, type, row) {
-                return data+" VND";
-        }  },
-            {
-                "data": 'status', "title": "Trạng thái",
-                "render": function (data, type, row) {
-                    if (data == 1) {
-                        return '<span class="badge badge-pill badge-primary" style="padding:10px;background-color: #1967d2;border-color: #1967d2;" >Kinh doanh</span>';
-                    } else {
-                        return '<span class="badge badge-pill badge-danger" style="padding:10px;">Ngừng kinh doanh</span>';
-                    }
-                }
-            },
-            {
-                "title": "Thao tác",
-                "render": function () {
-                    return '<td><a class="btn btn-primary" style="background-color: #1967d2;border-color: #1967d2;" id="btn"><i class="fa fa-wrench" aria-hidden="true"></i></a></td>';
-                }
-            },
-        ],
-        rowCallback: function(row, data) {
-            $(row).find('td').css('vertical-align', 'middle');
-          },
-          "language": {
-            "sInfo": "Hiển thị _START_ đến _END_ của _TOTAL_ bản ghi",
-            "lengthMenu": "Hiển thị _MENU_ bản ghi",
-            "sSearch": "Tìm kiếm:",
-            "sInfoFiltered": "(lọc từ _MAX_ bản ghi)",
-            "sInfoEmpty": "Hiển thị 0 đến 0 trong 0 bản ghi",
-            "sZeroRecords": "Không có data cần tìm",
-            "sEmptyTable": "Không có data trong bảng",
-            "oPaginate": {
-                "sFirst": "Đầu",
-                "sLast": "Cuối",
-                "sNext": "Tiếp",
-                "sPrevious": "Trước"
-            },
+      },
+      {
+        data: "productImages[0].imageUrl",
+        title: "Ảnh",
+        render: function (data, type, row) {
+          return `<img src="https://localhost:44328/${data}" alt="" style="border-radius: 10%;" width=120px height=110px>`;
+        },
+      },
+      { data: "name", title: "Tên sản phẩm" },
+      {
+        data: "retailPrice",
+        title: "Giá bán",
+        render: function (data, type, row) {
+          return data + " VND";
+        },
+      },
+      {
+        data: "status",
+        title: "Trạng thái",
+        render: function (data, type, row) {
+          if (data == 1) {
+            return '<span class="badge badge-pill badge-primary" style="padding:10px;background-color: #1967d2;border-color: #1967d2;" >Kinh doanh</span>';
+          } else {
+            return '<span class="badge badge-pill badge-danger" style="padding:10px;">Ngừng kinh doanh</span>';
           }
-    });
-    $('#productData tbody').on('click', 'tr', function (e) {
-        e.preventDefault();
-        let productId = $('#productData').DataTable().row(this).data().id;
-        if (productId !== null) {
-            localStorage.setItem("productId", productId);
-            window.location.href = `/frontend/admin/product-detail.html`;
-        }
-    });
+        },
+      },
+      {
+        title: "Thao tác",
+        render: function () {
+          return '<td><a class="btn btn-primary" style="background-color: #1967d2;border-color: #1967d2;" id="btn"><i class="fa fa-wrench" aria-hidden="true"></i></a></td>';
+        },
+      },
+    ],
+    rowCallback: function (row, data) {
+      $(row).find("td").css("vertical-align", "middle");
+    },
+    language: {
+      sInfo: "Hiển thị _START_ đến _END_ của _TOTAL_ bản ghi",
+      lengthMenu: "Hiển thị _MENU_ bản ghi",
+      sSearch: "Tìm kiếm:",
+      sInfoFiltered: "(lọc từ _MAX_ bản ghi)",
+      sInfoEmpty: "Hiển thị 0 đến 0 trong 0 bản ghi",
+      sZeroRecords: "Không có data cần tìm",
+      sEmptyTable: "Không có data trong bảng",
+      oPaginate: {
+        sFirst: "Đầu",
+        sLast: "Cuối",
+        sNext: "Tiếp",
+        sPrevious: "Trước",
+      },
+    },
+  });
+  $("#productData tbody").on("click", "tr", function (e) {
+    e.preventDefault();
+    let productId = $("#productData").DataTable().row(this).data().id;
+    if (productId !== null) {
+      localStorage.setItem("productId", productId);
+      window.location.href = `/frontend/admin/product-detail.html`;
+    }
+  });
 });
 //add event click datatable
 
-$('#productData tbody').on('click', 'tr', function (event) {
-    if ($(event.target).is('td')) {
-        window.location.href = "/frontend/admin/product-detail.html";
-    }
+$("#productData tbody").on("click", "tr", function (event) {
+  if ($(event.target).is("td")) {
+    window.location.href = "/frontend/admin/product-detail.html";
+  }
 });
 
 var option_category = [];
 $.getJSON("https://localhost:44328/api/Categories", function (result) {
-    for (var i = 0; i < result.length; i++) {
-        option_category.push('<option value="', result[i].id, '">', result[i].name, '</option>');
-    }
-    $("#category-select").html(option_category.join(''));
+  for (var i = 0; i < result.length; i++) {
+    option_category.push(
+      '<option value="',
+      result[i].id,
+      '">',
+      result[i].name,
+      "</option>"
+    );
+  }
+  $("#category-select").html(option_category.join(""));
 });
 
 var option_color = [];
 $.getJSON("https://localhost:44328/api/Color/Get", function (result) {
-    for (var i = 0; i < result.length; i++) {
-        option_color.push('<option value="', result[i].id, '">', result[i].name, '</option>');
-    }
-    $("#color-select").html(option_color.join(''));
+  for (var i = 0; i < result.length; i++) {
+    option_color.push(
+      '<option value="',
+      result[i].id,
+      '">',
+      result[i].name,
+      "</option>"
+    );
+  }
+  $("#color-select").html(option_color.join(""));
 });
-
