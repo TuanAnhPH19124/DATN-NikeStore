9cd130dbc5cc3130b2ab1e5b1d5dd385214488aa
Nguyen Tuan Anh <savanttkdt@gmail.com>
2023-09-09 03:04:23 +0700

    updating: call api màn product cửa user

--- frontend/customer/src/assets/controller/productController.js
+++ frontend/customer/src/assets/controller/productController.js
@@ -1,31 +1,57 @@
 (function () {
-    var productController = function (e, l, productService, categoryService, apiUrl) {
+    var productController = function (sizeService,colorService, e, l, productService, categoryService, apiUrl) {
         e.products = [];
         e.categories = [];
         e.sale = false;
         e.sortBy = "";
         e.genders = [
-            {id: 0, name: "Nam", selected: false},
-            {id: 1, name: "Nữ", selected: false},
-            {id: 2, name: "Unisex", selected: false},
+            { id: 0, name: "Nam", selected: false },
+            { id: 1, name: "Nữ", selected: false },
+            { id: 2, name: "Unisex", selected: false },
         ]
-        e.onSelectChange = function() {
+        e.colors = [];
+        e.sizes = [];
+        e.priceRanges = [
+            {min: 0, max: 1000000, selected: false},
+            {min: 1000000, max: 2000000, selected: false},
+            {min: 2000000, max: 4999999, selected: false},
+            {min: 5000000, max: 5000000 * 9999, selected: false}
+        ]
+
+
+
+        e.onSelectChange = function () {
             console.log('run filter');
             getProductsByFilter();
         };
 
-        function getProductsByFilter(){
+        function getProductsByFilter() {
+            var min = 5000000 * 9999;
+            var max = 0;
+            e.priceRanges.filter(item => {
+                if (item.selected){
+                    if (min > item.min)
+                        min = item.min;
+                    // 1. min = 0
+                    // 2. item.min = 10
+                    max = item.max;
+                };
+            });
             let params = {};
             params.sortBy = e.sortBy;
             params.sale = true;
             params.categories = e.categories.filter(item => item.selected).map(selected => selected.id);
+            params.colors = e.colors.filter(item => item.selected).map(selected => selected.id);
+            params.sizes = e.sizes.filter(item => item.selected).map(selected => selected.id);
+            params.min = min >= 0 ? min : null;
+            params.max = max > 0 ? max : null;
             productService.getProductsByParams(params)
-            .then(function (response){
-                e.products = response.data;
-            })
-            .catch(function (data){
-                console.log(data);
-            });
+                .then(function (response) {
+                    e.products = response.data;
+                })
+                .catch(function (data) {
+                    console.log(data);
+                });
         }
 
         e.countColors = function (array) {
@@ -47,11 +73,11 @@
             }
         }
 
-        e.calculatePercenOff = function (discount, price){
-            return discount/price * 100;
+        e.calculatePercenOff = function (discount, price) {
+            return 100 - discount / price * 100;
         }
 
-        e.getImgUrl = function (path){
+        e.getImgUrl = function (path) {
             const imgUrl = new URL(path, apiUrl);
             return imgUrl.href;
         }
@@ -61,26 +87,47 @@
                 .then(function (response) {
                     e.products = response.data;
                 })
-                .catch(function (data){
+                .catch(function (data) {
                     console.log(data);
                 });
-            
-            categoryService.getCategories()
-            .then(function (response){
-                e.categories = response.data;
-                e.categories.forEach(function (item){
+
+            colorService.getAllColor()
+            .then(function(response){
+                e.colors = response.data;
+                e.colors.forEach(item => {
+                    item.selected = false;
+                });
+                console.log(e.colors);
+            }, function(response){
+                console.error(response.data);
+            })
+
+            sizeService.getAllSize()
+            .then(function(response){
+                e.sizes = response.data;
+                e.sizes.forEach(item => {
                     item.selected = false;
                 })
+            },function(response){
+                console.error(response.data);
             })
-            .catch(function (data){
-                console.log(data);
-            });
+
+            categoryService.getCategories()
+                .then(function (response) {
+                    e.categories = response.data;
+                    e.categories.forEach(function (item) {
+                        item.selected = false;
+                    })
+                })
+                .catch(function (data) {
+                    console.log(data);
+                });
         }
 
 
         constructor();
     };
 
-    productController.$inject = ['$scope', '$location', 'productService', 'categoryService','apiUrl'];
+    productController.$inject = ['sizeService','colorService', '$scope', '$location', 'productService', 'categoryService', 'apiUrl'];
     angular.module("app").controller("productController", productController);
 }())
